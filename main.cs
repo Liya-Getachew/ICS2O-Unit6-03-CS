d819d5a9970b463c730ca0c4f9ea31dd9a8a80b9
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2022-12-16 17:09:16 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+class Program {
+    public static async Task Main()
+    {
+        HttpClient client = new HttpClient();
+        string response = await client.GetStringAsync("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5");
+        // Console.WriteLine(response);
+        var jsonAsDictionary = System.Text.Json.JsonSerializer.Deserialize<Object>(response);
+        // Console.WriteLine(jsonAsDictionary);
+        Console.WriteLine("");
+        JsonNode forecastNode = JsonNode.Parse(response)!;
+        // Console.WriteLine(forecastNode);
+        JsonNode weatherNode = forecastNode!["weather"][0]!;
+        // Console.WriteLine(weatherNode);
+        JsonNode main = weatherNode!["main"]!;
+        // Console.WriteLine(main1Node);
+        Console.WriteLine(main);
+        Console.WriteLine("");
+        // Console.WriteLine(forecastNode);
+        JsonNode main1Node = forecastNode!["main"]!;
+        // Console.WriteLine(mainNode);
+        JsonNode temp = main1Node!["temp"]!;
+        JsonNode feel = main1Node!["feels_like"]!;
+        Console.WriteLine(temp + " K, feels like " + feel + " K");
+        // Console.WriteLine(mainNode);
+        JsonNode humidity = main1Node!["humidity"]!;
+        Console.WriteLine("Humidity: " + humidity + "%");
+    }
+}
